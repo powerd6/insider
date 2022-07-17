3dc8adf0847334ec22dd02b9b3880881cb619373
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2022-07-17 13:07:56 +0200

    setup netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  adapter: netlify()
 });
