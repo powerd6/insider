f028d5b67a69372a8f06489a5fbd4246d7227c3e
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2022-07-17 21:58:54 +0200

    remove netlify ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: netlify(),
 });
