a840bcecebb2cf2172b86e394e24c3a42eb31b18
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2022-07-17 13:49:44 +0200

    remove sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
-  adapter: netlify()
+  integrations: [tailwind()],
+  adapter: netlify(),
 });
