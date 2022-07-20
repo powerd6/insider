3e72178f75533de455951bb42e4f5c24ad223906
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2022-07-20 21:23:54 +0200

    fix sliding visuals and reduce popping

--- src/components/sidebar/StatefulSidebar.jsx
+++ src/components/sidebar/StatefulSidebar.jsx
@@ -1,10 +1,18 @@
 import { useStore } from "@nanostores/preact";
 import { isSidebarOpen } from "@states/sidebar";
+import moutString from 'mout/string.js';
 
-export default function StatefulSidebar({ title, postsByCategory }) {
+const { trim } = moutString;
+
+export default function StatefulSidebar({ title, postsByCategory, currentPage }) {
   const $isSidebarOpen = useStore(isSidebarOpen);
-  return <aside className={$isSidebarOpen ? "visible" : "hidden"} class="mt-6 transition-all duration-500">
-    <nav class="sticky bg-gray-100 p-2 w-60 h-full">
+
+  function isCurrentPage(href) {
+    return trim(currentPage, ['/', ' ']) == trim(href, ['/', ' ']);
+  }
+
+  return <aside class={`relative bg-gray-100 transition-all duration-500 p-2 w-52 h-full rounded-r-xl ${$isSidebarOpen ? "left-0" : "-left-52 -ml-52"}`}>
+    <nav class=" mt-6 ">
       <h1 class="text-xl p-2">{title}</h1>
       <ol class="list-none p-2 grid grid-cols-1 gap-4">
         {Object.entries(postsByCategory).map(([category, posts]) => {
@@ -13,7 +21,10 @@ export default function StatefulSidebar({ title, postsByCategory }) {
             <ul class="pl-4">
               {posts.map(post =>
                 <li>
-                  <a href={post.url} class="text-blue-500 hover:text-blue-700 hover:underline">{post.title}</a>
+                  {
+                    isCurrentPage(post.url) ?
+                      <span href={post.url} class="text-pink-500 hover:text-pink-700 hover:underline">{post.title}</span> :
+                      <a href={post.url} class="text-blue-500 hover:text-blue-700 hover:underline">{post.title}</a>}
                 </li>
               )}
             </ul>
